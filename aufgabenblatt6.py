466caad3ffbd7a51119ff3fdc0e0bad19ee1d4dd
SarahClavadetscher <62946189+SarahClavadetscher@users.noreply.github.com>
2021-04-26 09:35:29 +0200

    Update aufgabenblatt6.py

--- aufgabenblatt6.py
+++ aufgabenblatt6.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
 owidDataPath = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 owidData = pd.read_csv(owidDataPath)
 situation = pd.DataFrame(owidData)
 
-print(situation)
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Test Title")
+])
