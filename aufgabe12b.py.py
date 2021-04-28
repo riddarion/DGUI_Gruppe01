ebdb81c275bafad747c0b427d9d2f4a054cac5f8
SarahClavadetscher <62946189+SarahClavadetscher@users.noreply.github.com>
2021-04-28 17:16:38 +0200

    Update aufgabe12b.py.py

--- aufgabe12b.py.py
+++ aufgabe12b.py.py
@@ -10,10 +10,10 @@ import plotly.express as px
 
 from statistics import mean
 
-df = pd.read_csv("C:\\Users\\Sarah\Documents\\Studium\\DGUI\\owid-covid-data.csv")
+
+df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 df.replace('', 0, inplace=True)
 df.replace(np.NaN, 0, inplace=True)
-
 
 keep_col = ["continent", "location", "total_cases", "hosp_patients", "new_cases", "new_vaccinations"]
 new_df = df[keep_col]
@@ -21,12 +21,12 @@ new_df = df[keep_col]
 dfma = new_df.groupby(['location', 'continent'])   [['total_cases', 'new_cases', 'hosp_patients', 'new_vaccinations']].   mean()
 print(dfma) 
 
-flat = dfma.reset_index() 
+dfma_f = dfma.reset_index() 
 
 
-fig = px.scatter_matrix(flat,
+fig = px.scatter_matrix(dfma_f,
     dimensions=["total_cases", "hosp_patients", "new_cases", "new_vaccinations"],
-                        color=flat.location)
+                        color=dfma_f.location)
 
 fig.show()
 
